1633057f7d245c01af82dab8fb333dc57d5fcdb7
Florian Pfaff <pfaff@kit.edu>
2023-07-12 01:03:48 +0200

    Automatically update import distribution imports

--- pyrecest/distributions/__init__.py
+++ pyrecest/distributions/__init__.py
@@ -1,63 +1,39 @@
-from .abstract_custom_distribution import AbstractCustomDistribution
-from .abstract_dirac_distribution import AbstractDiracDistribution
-from .abstract_disk_distribution import AbstractDiskDistribution
-from .abstract_ellipsoidal_ball_distribution import AbstractEllipsoidalBallDistribution
-from .abstract_periodic_distribution import AbstractPeriodicDistribution
-from .abstract_uniform_distribution import AbstractUniformDistribution
-from .circle.abstract_circular_distribution import AbstractCircularDistribution
-from .circle.circular_dirac_distribution import CircularDiracDistribution
-from .circle.circular_fourier_distribution import CircularFourierDistribution
-from .circle.custom_circular_distribution import CustomCircularDistribution
-from .circle.von_mises_distribution import VonMisesDistribution
-from .circle.wrapped_normal_distribution import WrappedNormalDistribution
-from .disk_uniform_distribution import DiskUniformDistribution
-from .ellipsoidal_ball_uniform_distribution import EllipsoidalBallUniformDistribution
-from .hypersphere_subset.abstract_hemispherical_distribution import (
-    AbstractHemisphericalDistribution,
-)
-from .hypersphere_subset.abstract_hyperhemispherical_distribution import (
-    AbstractHyperhemisphericalDistribution,
-)
-from .hypersphere_subset.abstract_hypersphere_subset_distribution import (
-    AbstractHypersphereSubsetDistribution,
-)
-from .hypersphere_subset.abstract_hypersphere_subset_uniform_distribution import (
-    AbstractHypersphereSubsetUniformDistribution,
-)
-from .hypersphere_subset.abstract_hyperspherical_distribution import (
-    AbstractHypersphericalDistribution,
-)
-from .hypersphere_subset.bingham_distribution import BinghamDistribution
-from .hypersphere_subset.custom_hemispherical_distribution import (
-    CustomHemisphericalDistribution,
-)
-from .hypersphere_subset.custom_hyperhemispherical_distribution import (
-    CustomHyperhemisphericalDistribution,
-)
-from .hypersphere_subset.hyperhemispherical_uniform_distribution import (
-    HyperhemisphericalUniformDistribution,
-)
-from .hypersphere_subset.hyperhemispherical_watson_distribution import (
-    HyperhemisphericalWatsonDistribution,
-)
-from .hypersphere_subset.hyperspherical_mixture import HypersphericalMixture
-from .hypersphere_subset.hyperspherical_uniform_distribution import (
-    HypersphericalUniformDistribution,
-)
-from .hypersphere_subset.von_mises_fisher_distribution import VonMisesFisherDistribution
-from .hypersphere_subset.watson_distribution import WatsonDistribution
-from .hypertorus.abstract_hypertoroidal_distribution import (
-    AbstractHypertoroidalDistribution,
-)
-from .hypertorus.abstract_toroidal_distribution import AbstractToroidalDistribution
-from .hypertorus.hypertoroidal_dirac_distribution import HypertoroidalDiracDistribution
+import importlib
+
+# Import the classes in the current directory
+from pyrecest.distributions.abstract_custom_distribution import AbstractCustomDistribution
+from pyrecest.distributions.abstract_dirac_distribution import AbstractDiracDistribution
+from pyrecest.distributions.abstract_disk_distribution import AbstractDiskDistribution
+from pyrecest.distributions.abstract_ellipsoidal_ball_distribution import AbstractEllipsoidalBallDistribution
+from pyrecest.distributions.abstract_periodic_distribution import AbstractPeriodicDistribution
+from pyrecest.distributions.abstract_uniform_distribution import AbstractUniformDistribution
+
+# Import the classes in the circle subdirectory
+circle = importlib.import_module("pyrecest.distributions.circle")
+globals().update(circle.__dict__)
+
+# Import the classes in the hypersphere_subset subdirectory
+hypersphere_subset = importlib.import_module("pyrecest.distributions.hypersphere_subset")
+globals().update(hypersphere_subset.__dict__)
+
+# Import the classes in the hypertorus subdirectory
+hypertorus = importlib.import_module("pyrecest.distributions.hypertorus")
+globals().update(hypertorus.__dict__)
+
+# Import the classes in the nonperiodic subdirectory
+nonperiodic = importlib.import_module("pyrecest.distributions.nonperiodic")
+globals().update(nonperiodic.__dict__)
+
+
 from .hypertorus.hypertoroidal_wrapped_normal_distribution import (
     HypertoroidalWrappedNormalDistribution,
 )
+from .circle.wrapped_normal_distribution import WrappedNormalDistribution
+from .hypertorus.hypertoroidal_dirac_distribution import HypertoroidalDiracDistribution
+from .circle.circular_dirac_distribution import CircularDiracDistribution
 from .hypertorus.toroidal_dirac_distribution import ToroidalDiracDistribution
-from .nonperiodic.abstract_linear_distribution import AbstractLinearDistribution
-from .nonperiodic.custom_linear_distribution import CustomLinearDistribution
-from .nonperiodic.gaussian_distribution import GaussianDistribution
+from .circle.von_mises_distribution import VonMisesDistribution
+from .hypersphere_subset.von_mises_fisher_distribution import VonMisesFisherDistribution
 
 # Aliases for brevity and compatibility with libDirectional
 HypertoroidalWNDistribution = HypertoroidalWrappedNormalDistribution
@@ -69,47 +45,17 @@ WDDistribution = CircularDiracDistribution
 VMFDistribution = VonMisesFisherDistribution
 
 __all__ = [
-    "CustomCircularDistribution",
-    "HypertoroidalWrappedNormalDistribution",
-    "VMDistribution",
-    "HypertoroidalWDDistribution",
-    "ToroidalWDDistribution",
-    "VMFDistribution",
-    "WDDistribution",
-    "WNDistribution",
-    "AbstractHemisphericalDistribution",
-    "CustomLinearDistribution",
-    "HyperhemisphericalWatsonDistribution",
-    "AbstractLinearDistribution",
-    "AbstractCircularDistribution",
+    "AbstractCustomDistribution",
     "AbstractDiracDistribution",
     "AbstractDiskDistribution",
     "AbstractEllipsoidalBallDistribution",
-    "AbstractHyperhemisphericalDistribution",
-    "AbstractHypersphereSubsetDistribution",
-    "AbstractHypersphereSubsetUniformDistribution",
-    "AbstractHypersphericalDistribution",
-    "AbstractHypertoroidalDistribution",
     "AbstractPeriodicDistribution",
-    "AbstractToroidalDistribution",
     "AbstractUniformDistribution",
-    "BinghamDistribution",
-    "AbstractCustomDistribution",
-    "CustomHemisphericalDistribution",
-    "CustomHyperhemisphericalDistribution",
-    "DiskUniformDistribution",
-    "EllipsoidalBallUniformDistribution",
-    "CircularFourierDistribution",
-    "GaussianDistribution",
-    "HyperhemisphericalUniformDistribution",
-    "HypersphericalMixture",
-    "HypersphericalUniformDistribution",
-    "HypertoroidalWDDistribution",
     "HypertoroidalWNDistribution",
+    "WNDistribution",
+    "HypertoroidalWDDistribution",
     "ToroidalWDDistribution",
-    "VonMisesFisherDistribution",
-    "VonMisesDistribution",
-    "WatsonDistribution",
-    "CircularDiracDistribution",
-    "WrappedNormalDistribution",
-]
+    "VMDistribution",
+    "WDDistribution",
+    "VMFDistribution"
+] + circle.__all__ + hypersphere_subset.__all__ + hypertorus.__all__ + nonperiodic.__all__
