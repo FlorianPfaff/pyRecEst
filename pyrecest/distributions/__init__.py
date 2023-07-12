bc279e385f30885bdc37ee0b6c86aba5d1055abf
Florian Pfaff <pfaff@kit.edu>
2023-07-12 07:15:49 +0200

    Imports without explicit enumeration

--- pyrecest/distributions/__init__.py
+++ pyrecest/distributions/__init__.py
@@ -1,72 +1,29 @@
-from .abstract_custom_distribution import AbstractCustomDistribution
-from .abstract_dirac_distribution import AbstractDiracDistribution
-from .abstract_disk_distribution import AbstractDiskDistribution
-from .abstract_ellipsoidal_ball_distribution import AbstractEllipsoidalBallDistribution
-from .abstract_periodic_distribution import AbstractPeriodicDistribution
-from .abstract_uniform_distribution import AbstractUniformDistribution
-from .circle.abstract_circular_distribution import AbstractCircularDistribution
-from .circle.circular_dirac_distribution import CircularDiracDistribution
-from .circle.circular_fourier_distribution import CircularFourierDistribution
-from .circle.custom_circular_distribution import CustomCircularDistribution
-from .circle.von_mises_distribution import VonMisesDistribution
-from .circle.wrapped_normal_distribution import WrappedNormalDistribution
-from .disk_uniform_distribution import DiskUniformDistribution
-from .ellipsoidal_ball_uniform_distribution import EllipsoidalBallUniformDistribution
-from .hypersphere_subset.abstract_hemispherical_distribution import (
-    AbstractHemisphericalDistribution,
-)
-from .hypersphere_subset.abstract_hyperhemispherical_distribution import (
-    AbstractHyperhemisphericalDistribution,
-)
-from .hypersphere_subset.abstract_hypersphere_subset_distribution import (
-    AbstractHypersphereSubsetDistribution,
-)
-from .hypersphere_subset.abstract_hypersphere_subset_uniform_distribution import (
-    AbstractHypersphereSubsetUniformDistribution,
-)
-from .hypersphere_subset.abstract_hyperspherical_distribution import (
-    AbstractHypersphericalDistribution,
-)
-from .hypersphere_subset.bingham_distribution import BinghamDistribution
-from .hypersphere_subset.custom_hemispherical_distribution import (
-    CustomHemisphericalDistribution,
-)
-from .hypersphere_subset.custom_hyperhemispherical_distribution import (
-    CustomHyperhemisphericalDistribution,
-)
-from .hypersphere_subset.hyperhemispherical_uniform_distribution import (
-    HyperhemisphericalUniformDistribution,
-)
-from .hypersphere_subset.hyperhemispherical_watson_distribution import (
-    HyperhemisphericalWatsonDistribution,
-)
-from .hypersphere_subset.hyperspherical_mixture import HypersphericalMixture
-from .hypersphere_subset.hyperspherical_uniform_distribution import (
-    HypersphericalUniformDistribution,
-)
-from .hypersphere_subset.von_mises_fisher_distribution import VonMisesFisherDistribution
-from .hypersphere_subset.watson_distribution import WatsonDistribution
-from .hypertorus.abstract_hypertoroidal_distribution import (
-    AbstractHypertoroidalDistribution,
-)
-from .hypertorus.abstract_toroidal_distribution import AbstractToroidalDistribution
-from .hypertorus.hypertoroidal_dirac_distribution import HypertoroidalDiracDistribution
-from .hypertorus.hypertoroidal_wrapped_normal_distribution import (
-    HypertoroidalWrappedNormalDistribution,
-)
-from .hypertorus.toroidal_dirac_distribution import ToroidalDiracDistribution
-from .nonperiodic.abstract_linear_distribution import AbstractLinearDistribution
-from .nonperiodic.custom_linear_distribution import CustomLinearDistribution
-from .nonperiodic.gaussian_distribution import GaussianDistribution
+import pkgutil
+import importlib
 
+# Iterate over each module in the current package
+for loader, module_name, is_pkg in pkgutil.walk_packages(__path__):
+    # If it's a package, skip it
+    if is_pkg:
+        continue
+
+    # Form the full module name (e.g., 'pyrecest.distributions.module_name')
+    full_module_name = f"{__name__}.{module_name}"
+
+    # Import the module and add it to globals
+    module = importlib.import_module(full_module_name)
+    globals().update({n: getattr(module, n) for n in module.__all__})
+
+# Now __all__ includes all symbols that any of the modules define in their __all__
+__all__ = list(globals().keys())
 # Aliases for brevity and compatibility with libDirectional
-HypertoroidalWNDistribution = HypertoroidalWrappedNormalDistribution
-WNDistribution = WrappedNormalDistribution
-HypertoroidalWDDistribution = HypertoroidalDiracDistribution
-ToroidalWDDistribution = ToroidalDiracDistribution
-VMDistribution = VonMisesDistribution
-WDDistribution = CircularDiracDistribution
-VMFDistribution = VonMisesFisherDistribution
+#HypertoroidalWNDistribution = HypertoroidalWrappedNormalDistribution
+#WNDistribution = WrappedNormalDistribution
+#HypertoroidalWDDistribution = HypertoroidalDiracDistribution
+#ToroidalWDDistribution = ToroidalDiracDistribution
+#VMDistribution = VonMisesDistribution
+#WDDistribution = CircularDiracDistribution
+#VMFDistribution = VonMisesFisherDistribution
 
 __all__ = [
     "CustomCircularDistribution",
