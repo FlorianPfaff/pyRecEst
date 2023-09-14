3ec8f8ef404c439d95ac2c0e327461ca367afec9
Florian Pfaff <pfaff@kit.edu>
2023-09-14 15:39:02 +0200

    Reordered some code for GaussianDistribution

--- pyrecest/distributions/nonperiodic/gaussian_distribution.py
+++ pyrecest/distributions/nonperiodic/gaussian_distribution.py
@@ -10,15 +10,15 @@ from .abstract_linear_distribution import AbstractLinearDistribution
 
 class GaussianDistribution(AbstractLinearDistribution):
     @beartype
-    def __init__(self, mu: np.ndarray, C: np.ndarray, check_validity=True):
-        AbstractLinearDistribution.__init__(self, dim=np.size(mu))
+    def __init__(self, mu: np.ndarray, C: np.ndarray, check_validity_cov=True):
+        assert np.ndim(mu) <= 1
+        AbstractLinearDistribution.__init__(self, dim=mu.shape[0])
+        self.mu = mu
+        
         assert (
             1 == np.size(mu) == np.size(C) or np.size(mu) == C.shape[0] == C.shape[1]
         ), "Size of C invalid"
-        assert np.ndim(mu) <= 1
-        self.mu = mu
-
-        if check_validity:
+        if check_validity_cov:
             if self.dim == 1:
                 assert C > 0, "C must be positive definite"
             elif self.dim == 2:
@@ -66,13 +66,13 @@ class GaussianDistribution(AbstractLinearDistribution):
         K = np.linalg.solve(self.C + other.C, self.C)
         new_mu = self.mu + np.dot(K, (other.mu - self.mu))
         new_C = self.C - np.dot(K, self.C)
-        return GaussianDistribution(new_mu, new_C, check_validity=False)
+        return GaussianDistribution(new_mu, new_C, check_validity_cov=False)
 
     def convolve(self, other):
         assert self.dim == other.dim
         new_mu = self.mu + other.mu
         new_C = self.C + other.C
-        return GaussianDistribution(new_mu, new_C, check_validity=False)
+        return GaussianDistribution(new_mu, new_C, check_validity_cov=False)
 
     def marginalize_out(self, dimensions):
         if isinstance(dimensions, int):  # Make it iterable if single integer
@@ -81,7 +81,7 @@ class GaussianDistribution(AbstractLinearDistribution):
         remaining_dims = [i for i in range(self.dim) if i not in dimensions]
         new_mu = self.mu[remaining_dims]
         new_C = self.C[np.ix_(remaining_dims, remaining_dims)]
-        return GaussianDistribution(new_mu, new_C, check_validity=False)
+        return GaussianDistribution(new_mu, new_C, check_validity_cov=False)
 
     def sample(self, n):
         return np.random.multivariate_normal(self.mu, self.C, n)
